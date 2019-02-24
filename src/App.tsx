c0d31cc9590b9161dcb0f0527cbd482969ec10d7
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-24 14:17:59 +0530

    drop react-hot-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import { hot } from 'react-hot-loader'
-
 import { Hello } from './Hello'
 
 export const App = () => <Hello />
-
-export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from '~/App'
+import { App } from '~/App'
 
-render(<App />, document.getElementById('root'))
+render(React.createElement(App), document.getElementById('root'))
